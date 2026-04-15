aceb08ad106b721f883a19fd90f20a9e65840628
nitink88 <nitink8807@gmail.com>
2026-04-15 12:04:52 +0530

    Add files via upload

---
+++ Assignment_16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n], even[n], odd[n];
+    int e = 0, o = 0;
+
+    // Input elements
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0)
+            even[e++] = arr[i];
+        else
+            odd[o++] = arr[i];
+    }
+
+    // Display even numbers
+    printf("\nEven numbers:\n");
+    for (i = 0; i < e; i++) {
+        printf("%d ", even[i]);
+    }
+
+    // Display odd numbers
+    printf("\nOdd numbers:\n");
+    for (i = 0; i < o; i++) {
+        printf("%d ", odd[i]);
+    }
+
+    return 0;
+}
